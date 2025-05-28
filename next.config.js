0a86f65642e87503bd387c4459fd8d747d9c9751
Kosuke Muramatsu <kousuke.muramatsu@gmail.com>
2025-05-28 23:57:41 +0900

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-
-  images: {
-    unoptimized: true,
+module.exports = {
+  eslint: {
+    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
-  
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+};
